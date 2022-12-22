a40260575a91fe3657e49a475a9b291b964635f8
Jay <35229@ma-web.nl>
2022-12-22 12:14:48 +0100

    update

--- canvashuis/app.js
+++ canvashuis/app.js
@@ -8,44 +8,102 @@ class App
         let g = canvasvariable.getContext("2d");
         g.beginPath()
         g.fillStyle = "red";
-        g.moveTo(300,100);
-        g.lineTo(700,200);
-        g.lineTo(600,400);
-        g.lineTo(200,300);
+        g.moveTo(30,10);
+        g.lineTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
         g.closePath();
         g.stroke();
         g.fill()
 
         g.beginPath()
         g.fillStyle = "grey";
-        g.moveTo(200,300)
-        g.lineTo(200,500)
-        g.lineTo(600,600)
-        g.lineTo(600,400)
+        g.moveTo(20,30)
+        g.lineTo(20,50)
+        g.lineTo(60,60)
+        g.lineTo(60,40)
         g.closePath()
         g.stroke()
         g.fill()
 
         g.beginPath()
         g.fillStyle = "grey";
-        g.moveTo(600,400)
-        g.lineTo(600,600)
-        g.lineTo(800,500)
-        g.lineTo(800,300)
+        g.moveTo(60,40)
+        g.lineTo(60,60)
+        g.lineTo(80,50)
+        g.lineTo(80,30)
         g.closePath()
         g.stroke()
         g.fill()
 
         g.beginPath()
         g.fillStyle = "grey";
-        g.moveTo(700,200)
-        g.lineTo(600,400)
-        g.lineTo(800,300)
+        g.moveTo(70,20)
+        g.lineTo(60,40)
+        g.lineTo(80,30)
         g.closePath()
         g.stroke()
         g.fill()
 
-        
+        g.beginPath()
+        g.fillStyle = "grey";
+        let randomGetal = Math.random();
+        if (randomGetal > 0.2 && randomGetal < 0.6){
+            g.fillStyle = "yellow";
+        }
+        g.moveTo(30,40);
+        g.lineTo(32.5,40);
+        g.lineTo(32.5,42.5);
+        g.lineTo(30,42.5);
+        g.lineTo(30,40);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath()
+        g.fillStyle = "grey";
+        randomGetal = Math.random();
+        if (randomGetal > 0.2 && randomGetal < 0.6){
+            g.fillStyle = "yellow";
+        }
+        g.moveTo(33,40);
+        g.lineTo(35.5,40);
+        g.lineTo(35.5,42.5);
+        g.lineTo(33,42.5);
+        g.lineTo(33,40);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath()
+        g.fillStyle = "grey";
+        randomGetal = Math.random();
+        if (randomGetal > 0.2 && randomGetal < 0.6){
+            g.fillStyle = "green";
+        }
+        g.moveTo(33,43);
+        g.lineTo(35.5,43);
+        g.lineTo(35.5,45.5);
+        g.lineTo(33,45.5);
+        g.lineTo(33,43);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.fillStyle = "grey";
+       randomGetal = Math.random();
+        if (randomGetal > 0.2 && randomGetal < 0.6){
+            g.fillStyle = "yellow";
+        }
+        g.moveTo(30,43);
+        g.lineTo(32.5,43);
+        g.lineTo(32.5,45.5);
+        g.lineTo(30,45.5);
+        g.lineTo(30,43);
+        g.closePath();
+        g.stroke();
+        g.fill()
        
     }
 }
---
+++ modules/app.js
@@ -0,0 +1,52 @@
+class App
+{
+    runApplication()
+    {
+        let canvasvariable=document.getElementById("canvasId")
+        console.log(canvasvariable)
+
+        let g = canvasvariable.getContext("2d");
+        g.beginPath()
+        g.fillStyle = "red";
+        g.moveTo(300,100);
+        g.lineTo(700,200);
+        g.lineTo(600,400);
+        g.lineTo(200,300);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath()
+        g.fillStyle = "grey";
+        g.moveTo(200,300)
+        g.lineTo(200,500)
+        g.lineTo(600,600)
+        g.lineTo(600,400)
+        g.closePath()
+        g.stroke()
+        g.fill()
+
+        g.beginPath()
+        g.fillStyle = "grey";
+        g.moveTo(600,400)
+        g.lineTo(600,600)
+        g.lineTo(800,500)
+        g.lineTo(800,300)
+        g.closePath()
+        g.stroke()
+        g.fill()
+
+        g.beginPath()
+        g.fillStyle = "grey";
+        g.moveTo(700,200)
+        g.lineTo(600,400)
+        g.lineTo(800,300)
+        g.closePath()
+        g.stroke()
+        g.fill()
+    }
+    
+}
+
+let app = new App();
+app.runApplication();
