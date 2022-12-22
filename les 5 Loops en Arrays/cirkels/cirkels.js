41064ad92483db85c85bafb09ab89bc92de2ab42
Jay <35229@ma-web.nl>
2022-12-22 12:33:03 +0100

    update

--- les 5 Loops en Arrays/cirkels/cirkels.js
+++ les 5 Loops en Arrays/cirkels/cirkels.js
@@ -11,8 +11,15 @@ ctx.beginPath();
 ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
 ctx.lineWidth = 3;
 ctx.strokeStyle = '#FF0000';
+ctx.fillStyle = '#FF0000';
 ctx.stroke();
 ctx.fill();
+}
+
+var times = 200;
+
+for(var i = 0; i < times; i++){
+    draw();
 }
 
 }
